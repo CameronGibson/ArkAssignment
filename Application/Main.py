c2b0a570f7da1cb645d6aa2ec5fc40ebb261de39
cameron <cameronkgibson28@gmail.com>
2021-01-21 18:46:29 -0500

    Removed the use of objects, Data is now persisting directly to the DB from the CSV files

--- Application/Main.py
+++ Application/Main.py
@@ -1,52 +1,58 @@
 import csv
-from Models.Product import Product
-from Models.Sales import Sales
-from Models.Salesman import Salesman
+import pyodbc
+import sys 
 
-productRecords = []
-salesRecords = []
-salesmanRecords = []
+#Open a connection to the DB and start reading the data straight from the CSV file into the DB.
+try:
+    connection = pyodbc.connect(
+        "Driver={SQL Server Native Client 11.0};"
+        "Server=desktop-m3p0ek9\localhost;"
+        "Database=Ark;"
+        "Trusted_Connection=yes")
+    
+    cursor = connection.cursor()
 
-def readData():
-    #Read in the data from the CSV Files.
-    print("Reading in data...")
+    print("Storing data in the database...")
 
-    #First we will read in ProductData.csv and store it in our Product object.
-    with open('Data\ProductData.csv', 'r') as file:
-        reader = csv.reader(file, delimiter = ',')
-        next(file)
+    #First we will read in SalesmanData.csv and store it in our Salesman table.
+    with open('Data\SalesmanData.csv', 'r') as salesmanFile:
+        reader = csv.reader(salesmanFile, delimiter = ',')
+        next(salesmanFile)
         for row in reader:
-            productRecords.append(Product(row[0], row[1], row[2]))
-            print(row)
-        file.close()
+            insertStatement = """INSERT INTO salesmanData VALUES (?, ?)"""
+            cursor.execute(insertStatement, row)
+            connection.commit()
+        salesmanFile.close()
 
-    #Next we will read in SalesData.csv and store it in our Sales object.
-    with open('Data\SalesData.csv', 'r') as file:
-        reader = csv.reader(file, delimiter = ',')
-        next(file)
+    #Next we will read in ProductData.csv and store it in our Product table.
+    with open('Data\ProductData.csv', 'r') as productsFile:
+        reader = csv.reader(productsFile, delimiter = ',')
+        next(productsFile)
         for row in reader:
-            salesRecords.append(Sales(row[0], row[1], row[2], row[3], row[4]))
-            print(row)
-        file.close()
+            insertStatement = """INSERT INTO productData VALUES (?, ?, ?)"""
+            cursor.execute(insertStatement, row)
+            connection.commit()
+        productsFile.close()
 
-     #First we will read in SalesmanData.csv and store it in our Salesman object.
-    with open('Data\SalesmanData.csv', 'r') as file:
-        reader = csv.reader(file, delimiter = ',')
-        next(file)
+    #Finally we will read in SalesData.csv and store it in our Sales table.
+    with open('Data\SalesData.csv', 'r') as salesFile:
+        reader = csv.reader(salesFile, delimiter = ',')
+        next(salesFile)
         for row in reader:
-            salesmanRecords.append(Salesman(row[0], row[1]))
-            print(row)
-        file.close()
+            insertStatement = """INSERT INTO salesData VALUES (?, ?, ?, ?, ?)"""
+            cursor.execute(insertStatement, row)
+            connection.commit()
+        salesFile.close()
 
-                  
+        #Close the connection.
+        connection.close()
 
+#if an exception occurs, throw an error and exit the program.
+except pyodbc.Error as ex:
+        sqlstate = ex.args[0]
+        if sqlstate == '28000':
+            sys.exit("\nAn error has occured while obtaining a connection to SQLServer... \nExiting the program gracefully.")         
 
-def getConnectionString(): 
-    #Get connection string values.
-    username = input("\nEnter username: ")
-    password = input("Enter password: ")
-    hostName = input("Enter host name: ")
-    databaseName = input("Enter database name: ")
-
-readData()
-getConnectionString()
+#upon completion of the date being persisted, close the cursor and and the connection before exiting the program.
+finally: 
+    sys.exit("\nTransaction complete: Exiting the program gracefully.")
